eabe0331dc3a5ea444341bf4969463e2659c202a
Antonio D'Averio <antonio.daverio@ares2t.com>
2024-11-12 15:37:19 +0100

    Fixed an accidental push of config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ INTEGRATION_TYPE_DEV = "Dev"
 # ATTENZIONE:
 # 1) quando si fa commit/push del codice, bisogna lasciare questa costante impostata su INTEGRATION_TYPE_PROD
 # 2) quando si sviluppa il codice, bisogna impostare questa costante su INTEGRATION_TYPE_DEV
-INTEGRATION_TYPE = INTEGRATION_TYPE_DEV
+INTEGRATION_TYPE = INTEGRATION_TYPE_PROD
